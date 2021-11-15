197eed92bd0d48efe71b49c79394d1ac8e8ff9d4
Evgeniya Kazakevich <evgeniya.kazakevich@ukg.com>
2021-11-15 14:35:03 +0300

    steps description added

--- src/main/java/api/service/BoardsService.java
+++ src/main/java/api/service/BoardsService.java
@@ -17,26 +17,27 @@ public class BoardsService extends TrelloService {
         return parser(getPostResponse(boardDto, GET_BOARDS_URL), new BoardDto());
     }
 
-    @Step
+    @Step(value="Get Board {0}")
     public BoardDto getBoard(String boardId) {
         return parser(getGetResponse(format(GET_BOARD_URL, boardId)), new BoardDto());
     }
 
-    @Step
+    @Step(value="Delete Board {0}")
     public void deleteBoard(String boardId) {
         getDeleteResponse(format(GET_BOARD_URL, boardId));
     }
 
-    @Step
+    @Step(value="Update Board {0}")
     public BoardDto updateBoard(BoardDto boardDto, String boardId) {
         return parser(getPutResponse(boardDto, format(GET_BOARD_URL, boardId)), new BoardDto());
     }
 
-    @Step
+    @Step(value="Get All Boards")
     public BoardDto[] getAllBoards(String memberId) {
         return parser(getGetResponse(format(GET_MEMBER_BOARDS_URL, memberId)), new BoardDto[] {});
     }
 
+    @Step(value="Get Board Id {0}")
     public String getBoardId(String boardName) throws Exception {
         return Arrays.stream(getAllBoards(getMemberId()))
                 .filter(x -> x.getName().equals(boardName))
--- src/main/java/api/service/ListsService.java
+++ src/main/java/api/service/ListsService.java
@@ -8,12 +8,12 @@ import io.qameta.allure.Step;
 
 public class ListsService extends TrelloService {
 
-    @Step
+    @Step(value="Create List {0}")
     public ListDto createList(ListDto listDto, String boardId) {
         return parser(getPostResponse(listDto, format(GET_BOARD_LISTS_URL, boardId)), new ListDto());
     }
 
-    @Step
+    @Step(value="Get Lists")
     public ListDto[] getLists(String boardId) {
         return parser(getGetResponse(format(GET_BOARD_LISTS_URL, boardId)), new ListDto[]{});
     }
